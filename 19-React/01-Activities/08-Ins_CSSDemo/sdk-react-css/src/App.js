c8ac1f7999a9aa21f6b71809a940f44c2ddb0a3d
Joe Adams <adams.jd10@gmail.com>
2020-07-09 17:47:46 -0700

    update

---
+++ 19-React/01-Activities/08-Ins_CSSDemo/sdk-react-css/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ 19-React/01-Activities/08-Ins_CSSDemo/sdk-react-css/src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Navbar;
