301f23d096db7c53986566f2df86cb868814a84d
Joe Adams <adams.jd10@gmail.com>
2020-07-11 10:04:40 -0700

    update

---
+++ 19-React/01-Activities/06-Ins_JSXVariables/sdk-jsx-variables/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JSXVariables from "./components/jsxvariables/JSXVariables";
+
+function App() {
+  return (
+    <>
+      <JSXVariables />
+    </>
+  );
+}
+
+export default App;
